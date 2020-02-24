f75493c404d2cacb82a082abc4b1028552eaf9de
Nidhirajput1301 <48066668+Nidhirajput1301@users.noreply.github.com>
2020-02-25 01:36:27 +0530

    Update and rename README.md to index.php

---
+++ index.php
@@ -0,0 +1,82 @@
+# deploy
+<!DOCTYPE html>
+<html lang="pt-br">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Responsive Sticky Navbar</title>
+      <link rel="stylesheet" href="all.css">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+      <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>    
+      <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+      <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+
+      <style>
+         table, th, td {
+           border-bottom: 1px solid #f12711;
+           border-collapse: collapse;
+           width: 100%;
+           
+         }
+         th, td {
+           padding: 15px;
+         }
+
+         tr:hover {background-color: #f5af19;}
+         </style>
+
+   </head>
+   <body>
+      <div class="wrapper">
+         <header>
+            <nav>
+               <div class="menu-icon">
+                  <i class="fa fa-bars fa-2x"></i>
+               </div>
+               <div class="logo">
+                  WELCOME
+               </div>
+               <div class="menu">
+                  <ul>
+                     <li><a href="lndf.html">Lost And Found</a></li>
+                     <li><a href="comp.html">Complaint</a></li>
+                     <li><a href="event.html">Library</a></li>
+                     <li><a href="cafe.html">Cafeteria</a></li>
+                  </ul>
+               </div>
+            </nav>
+         </header>
+         <div class="content" style="position: relative;">
+            <div style="width: 30%;  text-align: center;"><img src="img/avtar.jpg" alt="Diya" style="border-radius: 50%; height:300px;"></div>
+            <div style="width: 50%; height: 100%; position: absolute; top: 5px; right: 40px;">
+               <table>
+                  <tr>
+                    <td>Name</td>
+                    <td>Diya Jaiswal</td>
+                  </tr>
+                  <tr>
+                    <td>UserID</td>
+                    <td>18CSE011</td>
+                  </tr>
+                  <tr>
+                    <td>E-mail</td>
+                    <td>diya1301@gmail.com</td>
+                  </tr>
+                  <tr>
+                     <td>Branch</td>
+                     <td>CSE</td>
+                   </tr>
+                   <tr>
+                     <td>Year</td>
+                     <td>2nd</td>
+                   </tr>
+                </table>
+            </div>
+         </div>
+      </div>
+      
+   </body>
+</html>
